d14e71eec0cb6e12c0aea2b6bf65ed55df9f78a1
st4ff-dev <st4ff_developer@proton.me>
2025-06-04 13:01:31 +0200

    Практика | task 3

--- custom_logger.py
+++ custom_logger.py
@@ -3,6 +3,16 @@ from datetime import datetime
 
 
 
-def log(text: str):
-    time = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    sys.stderr.write(f"[{time}] {text}\n")
+class Logger:
+    def __init__(
+        self,
+        out_stream,
+        time_formatter
+    ) -> None:
+        self.out_stream = out_stream
+        self.time_formatter = time_formatter
+
+
+    def log(self, text: str):
+        time = datetime.now().strftime(self.time_formatter)
+        self.out_stream.write(f"[{time}] {text}\n")
